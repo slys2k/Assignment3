dc9afda0cc61bed572262e60e8df18c7bb59515c
slys2k <keithcyr@utexas.edu>
2014-02-25 13:52:53 -0600

    A3Driver
    Started to play with the driver layout.

--- A3Driver.java
+++ A3Driver.java
@@ -5,12 +5,52 @@ import java.util.*;
 public class A3Driver
 {
 	public static void main(String[] args) 
-	{
-		// TODO Auto-generated method stub
-		
-		//Open file; file name specified in args (command line)
-		
-		//Parse input, take appropriate actions.
+	{ 
+		if (args.length != 1) 
+		{
+			System.err.println ("Error: Incorrect number of command line" 
+									  "arguments");
+			System.exit(-1);
+		}
+		processLinesInFile (args[0]);
+	}
+
+	public static void processLinesInFile (String filename) 
+	{ 
+		// open file; file name specified in args (command line)
+		A3Driver shoppingCart = new A3Driver(); 
+		try 
+		{
+			FileReader freader = new FileReader(filename);
+			BufferedReader reader = new BufferedReader(freader);
+			
+			for (String s = reader.readLine(); s != null; s = reader.readLine()) 
+			{
+				// parse input, take appropriate actions.
+				
+				// get the first part of the line - use as a switch
+				// default on the switch is an error because falling through the 
+				// switch indicates that the first part of the input line was 
+				// incorrect
+				
+				// each switch statement will handle the individual command
+				
+				// print command will handle the sort action prior to printing
+			}
+		} 
+		catch (FileNotFoundException e) 
+		{
+			System.err.println ("Error: File not found. Exiting...");
+			e.printStackTrace();
+			System.exit(-1);
+		}
+		catch (IOException e) 
+		{
+			System.err.println ("Error: IO exception. Exiting...");
+			e.printStackTrace();
+			System.exit(-1);
+		}
+	}
 		
 		//Stub for arraylist.
 		ArrayList<Item> shoppingCart = new ArrayList<Item>(); 
@@ -24,7 +64,7 @@ public class A3Driver
 			temp.printAttributes();
 			//This (above) works because of polymorphism: a determination is made at runtime, 
 			//based on the inherited class type, as to which method is to be invoked. Eg: If it is an instance
-			// of Grocery, it will invoke the calculatePrice () method defined in Grocery.
+			// of Grocery, it will invoke the calcPrice () method defined in Grocery.
 		}		
 	}
 }
