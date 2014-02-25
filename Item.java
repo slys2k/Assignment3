af191ba1971930362e20d072da1537c484f63b78
slys2k <keithcyr@utexas.edu>
2014-02-25 09:02:58 -0600

    Class Item
    First Run at calculation methods.

--- Item.java
+++ Item.java
@@ -30,9 +30,18 @@ public class Item
 	
 	// Constants
 	
-	/** There are no constants yet defined for this class.
+	/** Current salex tax rate, in decimal format (e.g. 10% = .10)
 	  */
+	private static final int TAX_RATE = .10;
 	
+	/** Current shipping rate.
+	  */
+	  private static final int SHIPPING_RATE = 20;
+	  
+	/** Current premium shipping rate, in decimal format (e.g. 20% = .20)
+	  */
+	  private static final int PREMIUM_SHIPPING = .20;
+	  
 	// Constructors
 
 	/** Construct an Item with the following initial values.
@@ -57,39 +66,39 @@ public class Item
 	/** Assign a new name to the item in the cart.
 	  * @param newName The desired name.
 	  */
-	public void setName(String newName ) {	itemName = newName; }
+	public void setName(String newName) {	itemName = newName; }
 	
 	/** Change the price of one of these items to a desired value.
 	  * @param newPrice The desired price for this item.
 	  */
-	public void setPrice(float newPrice ) { itemPrice = newPrice; }
+	public void setPrice(float newPrice) { itemPrice = newPrice; }
 	
 	/** Change the quantity of an item in the cart.
 	  * @param newQuantity The new number of this item wished to be purchased.
 	  */
-	public void setQuantity(int newQuantity ) { itemQuantity = newQuantity; }
+	public void setQuantity(int newQuantity) { itemQuantity = newQuantity; }
 
 	/** Assigns a new weight to the item in the cart
 	  * @param newWeight The desired weight of the item in the cart
 	  */
-	public void setWeight(float newWeight ) { itemWeight = newWeight; }
+	public void setWeight(float newWeight) { itemWeight = newWeight; }
 
 	// Access Methods
 
 	/** Determines the name of the current item.
 	  * @return The the name of the object. 
 	  */
-	public String getName( ) { return itemName; }
+	public String getName() { return itemName; }
 	
 	/** Gets the price associated with one of these items.
 	  * @return The price per each.
 	  */
-	public float getPrice( ) { return itemPrice; }
+	public float getPrice() { return itemPrice; }
 	
 	/** Gets the quantity of this item currently in the cart.
 	  * @return The item quantity.
 	  */
-	public int getQuantity( ) { return itemQuantity; }
+	public int getQuantity() { return itemQuantity; }
 
 	/** Determines the weight assigned to one of these items.
 	  * @return The weight per each. 
@@ -97,16 +106,58 @@ public class Item
 	public float getWeight() { return itemWeight; }
 		
 	// Other Methods
-	
-	float calculatePrice () 
+
+	/** Determines the cost associated with shipping this item.
+	  * @return The shipping cost. 
+	  */
+	float calcShipping()
 	{
-		float final_price = 0;
-		// Insert price calculation here
-		return final_price;
+		float shipping_cost = 0;
+
+		float weight = getWeight();
+		int quantity = getQuantity;
+
+		shipping_cost = ((SHIPPING_RATE * weight) * quantity)
+
+		return shipping_cost
 	}
 	
+	/** Determines the cost associated with sales tax for this item.
+	  * @return The amount of sales tax. 
+	  */
+	float calcTax()
+	{
+		float tax_cost = 0;
 
-	void printItemAttributes () 
+		int quantity = getQuantity;
+		float price = getPrice
+
+		tax_cost = ((quantity * price) * TAX_RATE)
+
+		return tax_cost;
+	}
+	
+	/** Determines the entire cost of this item.
+	  * @return The final price for this item.
+	  */
+	float calcPrice () 
+	{
+		float final_price = 0;
+
+		float tax = calcTax();
+		float shipping = calcShipping();
+		int quantity = getQuantity;
+		float price = getPrice
+		
+		final_price = (quantity * price) + tax + shipping;
+
+		return final_price;
+	}
+
+	/** Determines the weight assigned to one of these items.
+	  * @return The weight per each. 
+	  */
+	void printAttributes () 
 	{
 		//Print all applicable attributes of this class
 	}
