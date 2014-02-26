a40d69e1c5e215454ff0ff1d72324dabfdf4f9e3
slys2k <keithcyr@utexas.edu>
2014-02-25 19:10:04 -0600

    Revert "Revert "A3""
    This reverts commit da3ef5de260481cd9e949676a677f6a0c4f3b3e4.

--- A3Driver.java
+++ A3Driver.java
@@ -12,8 +12,10 @@ public class A3Driver
 	private static float price = 0;
 	private static float weight = 0;
 	private static String name = null;
-	private static String option1 = null;
-	private static String Option2 = null;
+	private static boolean fragile = false;
+	private static boolean taxable = true;
+	private static boolean perishable = false;
+	private static char opCode = ' ';
 
 	public static void main(String[] args) 
 	{ 
@@ -46,7 +48,6 @@ public class A3Driver
 										// to check for validity
 				{
 					case 'I': // insert
-						char type = itemType;
 						if(type == 'C')
 						{
 							temp = new Clothing(name, price, qty, weight);
@@ -100,7 +101,6 @@ public class A3Driver
 	
 	public static String shopping (String s)
 	{
-		char opCode = 0;
 		String cat = "";
 		String of1 = "";
 		String of2 = "";
@@ -117,23 +117,38 @@ public class A3Driver
 				qty = Integer.parseInt(input[4]);
 				weight = Integer.parseInt(input[5]);
 			}
-			if(cat == "electronics")
+			else if(cat == "electronics")
 			{						
 				name = input[2];
 				price = Integer.parseInt(input[3]);
 				qty = Integer.parseInt(input[4]);
 				weight = Integer.parseInt(input[5]);
-				of1 = input[6];
-				of2 = input[7];
+
+				if(input[6] == "F")
+				{
+					fragile = true;
+				}
+				
+				if(input[7] == "TX" || input[7] == "NM" || input[7] == "VA" 
+						|| input[7] == "AZ" || input[7] == "AK");
+				{
+					taxable = false;
+				}
 			}
-			if(cat == "groceries")
+
+			else if(cat == "groceries")
 			{
 				name = input[2];
 				price = Integer.parseInt(input[3]);
 				qty = Integer.parseInt(input[4]);
 				weight = Integer.parseInt(input[5]);
-				of1 = input[6];						
+
+				if(input[6] == "P")
+				{
+					perishable = true;
+				}
 			}
+
 			else error();
 		}
 		else if(input[0] == "search")
